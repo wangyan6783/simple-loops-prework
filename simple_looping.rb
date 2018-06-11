f19a7a16ee8df7953b2c1345c99f03d7f2f02cc2
Yan Wang <wangyan6783@gmail.com>
2018-06-11 00:55:04 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   times = 0
   loop do
-    break if times < number_of_times
+    break if times >= number_of_times
     puts phrase
     times += 1
   end
